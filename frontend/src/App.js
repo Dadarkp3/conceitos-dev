50359759336de660bb14ff20e3b5f7e8c69082f6
Daiane Mascarenhas Lauro da Silva <adm.daianesilva@gmail.com>
2020-04-17 22:04:44 -0300

    webpack file-loader config for images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
+
 import './App.css';
+
+import background from './assets/background.jpeg' 
 
 export default function App(){
   const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web' ]);
@@ -12,6 +15,7 @@ export default function App(){
     return (
       <>
         <Header title="Projects"/>
+        <img width="300" src={background} alt="background"/>
         <ul>
           {projects.map(project => <li key={project}>{project}</li>)}
         </ul>
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -25,7 +25,13 @@ module.exports={
           { loader: 'style-loader' },
           { loader: 'css-loader' },
         ]
-      }
+      },
+      {
+        test: /.*\.(gif|png|jp?eg)$/i,
+        use: {
+          loader: 'file-loader'
+        }
+      },
     ]
   }
 };
